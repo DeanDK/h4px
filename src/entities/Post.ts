48eed6153d5d0b8b649f734ee925e8c2649e7fe4
DeanDK <deanbozic12@gmail.com>
2020-08-22 13:42:54 +0200

    mikro orm setup

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,29 +1,16 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core'
+
 @Entity()
-export class Book {
+export class Post {
+    @PrimaryKey()
+    id!: number
 
-  @PrimaryKey()
-  id!: number;
+    @Property({ type: 'date' })
+    createdAt = new Date()
 
-  @Property()
-  createdAt = new Date();
+    @Property({ type: 'date', onUpdate: () => new Date() })
+    updatedAt = new Date()
 
-  @Property({ onUpdate: () => new Date() })
-  updatedAt = new Date();
-
-  @Property()
-  title!: string;
-
-  @ManyToOne() // when you provide correct type hint, ORM will read it for you
-  author!: Author;
-
-  @ManyToOne(() => Publisher) // or you can specify the entity as class reference or string name
-  publisher?: Publisher;
-
-  @ManyToMany() // owning side can be simple as this!
-  tags = new Collection<BookTag>(this);
-
-  constructor(title: string, author: Author) {
-    this.title = title;
-    this.author = author;
-  }
-
+    @Property({ type: 'text' })
+    title!: string
+}
