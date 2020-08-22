1085aff83e6fb04c44b3c475a471252188bab5c9
DeanDK <deanbozic12@gmail.com>
2020-08-22 14:36:01 +0200

    added crud operations to post resolver

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,23 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core'
+import { ObjectType, Field, Int } from 'type-graphql'
 
+// Convert TS class to graphql type
+@ObjectType()
 @Entity()
 export class Post {
+    @Field(() => Int)
     @PrimaryKey()
     id!: number
 
+    @Field(() => String)
     @Property({ type: 'date' })
     createdAt = new Date()
 
+    @Field(() => String)
     @Property({ type: 'date', onUpdate: () => new Date() })
     updatedAt = new Date()
 
+    @Field()
     @Property({ type: 'text' })
     title!: string
 }
