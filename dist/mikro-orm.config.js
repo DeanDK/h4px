2a3d4480623cef592d0fdbe6f0ae36f2cbb0a26f
DeanDK <deanbozic12@gmail.com>
2020-08-22 22:17:45 +0200

    added user resolver login and registration mutations

--- dist/mikro-orm.config.js
+++ dist/mikro-orm.config.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const constants_1 = require("./constants");
 const Post_1 = require("./entities/Post");
 const path_1 = __importDefault(require("path"));
+const User_1 = require("./entities/User");
 exports.default = {
     migrations: {
         migrations: {
@@ -13,7 +14,7 @@ exports.default = {
             pattern: /^[\w-]+\d+\.[tj]s$/,
         },
     },
-    entities: [Post_1.Post],
+    entities: [Post_1.Post, User_1.User],
     dbName: 'lireddit',
     type: 'postgresql',
     debug: !constants_1.__prod__,
---
+++ src/entities/User.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core'
+import { ObjectType, Field, Int } from 'type-graphql'
+
+// Convert TS class to graphql type
+@ObjectType()
+@Entity()
+export class User {
+    @Field(() => Int)
+    @PrimaryKey()
+    id!: number
+
+    @Field(() => String)
+    @Property({ type: 'date' })
+    createdAt = new Date()
+
+    @Field(() => String)
+    @Property({ type: 'date', onUpdate: () => new Date() })
+    updatedAt = new Date()
+
+    @Field()
+    @Property({ type: 'text', unique: true })
+    username!: string
+
+    @Property({ type: 'text')
+    password!: string
+}
